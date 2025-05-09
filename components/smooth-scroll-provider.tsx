f3cb87b974c3eb669e0a82dae9b67eb48da8a5af
Rezezi <axcelrezezi@gmail.com>
2025-05-09 14:42:48 +0700

    ha

--- components/smooth-scroll-provider.tsx
+++ components/smooth-scroll-provider.tsx
@@ -12,10 +12,7 @@ export default function SmoothScrollProvider({ children }: SmoothScrollProviderP
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      smoothTouch: false,
+      // smooth: true, // Removed as it is not a valid property
       touchMultiplier: 2,
     })
 
